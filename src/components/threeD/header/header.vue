16409adb6cd63afccfa2c8da7906b4cf959cf9e4
Yorman Castellanos <yorman.castellanos@gmail.com>
2021-08-01 12:59:04 -0400

    header is starting

---
+++ src/components/threeD/header/header.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="header">
+    <div class="header-logo"></div>
+    <div class="header-nav">
+      <ul>
+        <li v-for="i in nav" @click="navClickHandler(i)" :key="i.title">{{ i.title }}</li>
+      </ul>
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: "header",
+  data() {
+    return {
+      nav: [
+        {
+          title: "Bio",
+          modelPosition: [0,0,0]
+        },
+        {
+          title: "Link 2",
+          modelPosition: [0,0,0]
+        },
+        {
+          title: "Link 3",
+          modelPosition: [0,0,0]
+        },
+        {
+          title: "Contact",
+          modelPosition: [0,0,0]
+        }
+      ]
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/threeD/threeD.vue
+++ src/components/threeD/threeD.vue
@@ -15,4 +15,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import '@/styles/global';
 </style>
