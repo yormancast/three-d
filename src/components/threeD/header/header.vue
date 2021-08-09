797c20feb449f8f9fabd8043c085e2a0ffb5873d
Yorman Castellanos <yorman.castellanos@gmail.com>
2021-08-09 02:52:03 -0400

    dot

--- src/components/threeD/header/header.vue
+++ src/components/threeD/header/header.vue
@@ -5,7 +5,13 @@
     </div>
     <div class="main-header-nav col-xs-12 col-sm">
       <div class="row">
-        <button  :class="[{active : i.visible}, 'col-xs-3 main-header-nav-list primary']" v-for="i in nav" @click="navClickHandler(i)" :key="i.title">{{ i.title }}</button>
+        <button
+          :class="[{active : i.visible}, 'col-xs-3 main-header-nav-list primary']" 
+          v-for="i in nav" 
+          @click="navClickHandler(i)" 
+          :key="i.title">
+            {{ i.title }}.
+          </button>
       </div>
     </div>
   </section>
