b8dfe95b39243a46fb1311d7264dd89885ff332d
Yorman <yormancastellanos@gmail.com>
2021-08-23 14:51:23 -0400

    clean up

--- src/components/threeD/socials/socials.vue
+++ src/components/threeD/socials/socials.vue
@@ -1,7 +1,7 @@
 <template>
    <section class="socials">
      <ul>
-       <li class="socials-icon"><img src="@/assets/img/profile.jpg" alt="profile"></li>
+       <li class="socials-icon"><a href="https://github.com/yormancast/three-d" target="_blank"><img src="@/assets/img/profile.jpg" alt="profile"></a></li>
        <li class="socials-icon"><a href="https://github.com/yormancast" target="_blank"><img style="background-color: #fff" src="@/assets/img/icons/iconmonstr-github-1.svg" alt="profile"></a></li>
        <li class="socials-icon"><a href="https://www.linkedin.com/in/yormancastellanos/?locale=en_US" target="_blank"><img style="background-color: #fff;" src="@/assets/img/icons/iconmonstr-linkedin-3.svg" alt="profile"></a></li>
      </ul>
